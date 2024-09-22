7d0bb3e5ffe1cecc21c1d3cb508b10156df5128b
Lutfor Rahman <lutforr3845@gmail.com>
2024-09-23 05:58:33 +0600

    Add utility js for common function

--- Js/script.js
+++ Js/script.js
@@ -22,3 +22,58 @@ historyBtn.addEventListener('click', function(){
     document.getElementById('history-section').classList.remove("hidden");
     changeActiveStatus(historyBtn,donateBtn);
 });
+
+/* common function for All "Money-Donate" button */
+function  moneyDonateBtnClick(inputId ,receivedAmountId,placeId ) {
+
+     const inputValue = inputField(inputId);
+     const donateAmount = parseFloat(inputValue.value);
+     const receivedAmount = receivedMoney(receivedAmountId);
+     const balance = parseFloat(document.getElementById('balance').innerText);
+
+     if(donateAmount< 0 ||  donateAmount> balance || isNaN(donateAmount)) {
+        inputValue.value = "";
+       return alert('Invalid Donate Amount');
+     }
+
+    if(donateAmount > 0 && donateAmount<= balance) {
+         const newBalance = balance - donateAmount;
+         document.getElementById('balance').innerText = newBalance;
+         const newReceivedAmount =  receivedAmount + donateAmount;
+         document.getElementById(receivedAmountId).innerText =  newReceivedAmount;
+        // showMpdal 
+        document.getElementById('my_modal_1').showModal()
+         inputValue.value = "";
+
+
+         
+         // Defind the place where donate money?
+         const place =  document.getElementById(placeId).innerText; 
+         
+         // Histosy section  update
+         const historyAdd = document.getElementById('history-section');
+         const historyItem = document.createElement('div');
+         historyItem.className = 'p-4 md:p-8 border border-[#1111111A] rounded-2xl shadow'
+         historyItem.innerHTML = `
+                <h1 class="pb-3 text-lg font-bold leading-5 md:text-xl text-txtPrimary">${donateAmount} Taka is ${place}</h1>
+                <p class="text-base font-light text-txtSecondary">Date : ${new Date()}</p>
+         `
+        historyAdd.insertBefore(historyItem, historyAdd.firstChild);
+
+        
+    }
+};
+    // ADD click  event listener to all "Money-Donate" buttons
+    moneyDonateBtn('donate-amount-btn1').addEventListener('click',function(){
+        moneyDonateBtnClick ('money-inputField1' , 'noakhali-received-money', 'place1');
+    })
+    moneyDonateBtn('donate-amount-btn2').addEventListener('click',function(){
+        moneyDonateBtnClick ('money-inputField2' , 'feni-received-money', 'place2');
+    })
+    moneyDonateBtn('donate-amount-btn3').addEventListener('click',function(){
+        moneyDonateBtnClick ('money-inputField3' , 'forInjuredPeople-received-money', 'place');
+    })
+
+
+
+
